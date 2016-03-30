ae83f488d7b8779d648375e5452e377a45072821
Sharon Dulava <sd1249@humboldt.edu>
2016-03-30 12:28:52 -0700

    updates to triplicate code, 3/30/2016 @12:27: added nn crosses to data for all flight combinations

--- triplicate_nn_analysis.R
+++ triplicate_nn_analysis.R
@@ -5,7 +5,7 @@ library(rgeos)
 library(sp)
 library(rgdal)
 library(PresenceAbsence)
-
+library(maptools)
 # Create spatial points data frames from flight 1, flight 3, and flight 4 shapefiles
 
 a <- readOGR(dsn="./TestData", layer="AWPE_F1_400_Bsouth")
@@ -35,14 +35,62 @@ b$dist <- pointsnnba$dist
 b$nnid <- a@data[pointsnnba$which,3]
 head(b) 
 
-################## TIM - what is the best way to approach this? 
+
 # from flight a to flight c
 pointsnnac <- nncross(pointsa, pointsc)
-a$dist <- pointsnnac$dist
-a$nnid <- c@data[pointsnnac$which,3]                             
-head(a) # "a" is the data for the flight from shapefile
+ac<-a
+ac$dist <- pointsnnac$dist
+ac$nnid <- c@data[pointsnnac$which,3]                             
+head(ac) 
 
+#from flight c to flight a 
+pointsnnca <- nncross(pointsc, pointsa)
+ca<-c
+ca$dist <- pointsnnca$dist
+ca$nnid <- a@data[pointsnnca$which,3]                             
+head(ca)
 
+#from flight b to flight c
+pointsnnbc <- nncross(pointsb, pointsc)
+bc<-b
+bc$dist <- pointsnnbc$dist
+bc$nnid <- c@data[pointsnnbc$which,3]
+head(bc) 
+
+#from flight c to flight b 
+pointsnncb <- nncross(pointsc, pointsb)
+cb<-c
+cb$dist <- pointsnncb$dist
+cb$nnid <- b@data[pointsnncb$which,3]                             
+head(cb)
+
+for(i in 1:length(unique_pts_a)){
+  # take a point from flight a
+  cur_bird <- flight_a_data[flight_a_data$Point_ID==unique_pts_a[i],]
+  
+  # identify its nearest neighbor in flight b
+  nn_a_b_bird <- cur_bird[cur_bird$Flight_To == flight_b,]$NN_Pt_ID
+  nn_a_c_bird <- cur_bird[cur_bird$Flight_To == flight_c,]$NN_Pt_ID
+  
+  b_bird <- flight_b_data[flight_b_data$Point_ID==nn_a_b_bird,]
+  nn_b_a_bird <- b_bird[b_bird$Flight_To == flight_a,]$NN_Pt_ID
+  nn_b_c_bird <- b_bird[b_bird$Flight_To == flight_c,]$NN_Pt_ID
+  
+  c_bird <- flight_c_data[flight_c_data$Point_ID==nn_a_c_bird,]
+  nn_c_a_bird <- c_bird[c_bird$Flight_To == flight_a,]$NN_Pt_ID
+  nn_c_b_bird <- c_bird[c_bird$Flight_To == flight_b,]$NN_Pt_ID
+  
+  # if ITS nearest neighbor is point from flight a, add 1 to our count of nesting birds
+  if(nn_a_b_bird == nn_c_b_bird && nn_a_c_bird == nn_b_c_bird && nn_b_a_bird == nn_c_a_bird 
+     && nn_b_a_bird == cur_bird$Point_ID[1] && nn_c_a_bird == cur_bird$Point_ID[1]
+     && nn_b_c_bird == c_bird$Point_ID[1] && nn_c_b_bird == b_bird$Point_ID[1]){
+    nesting_birds <- nesting_birds + 1
+    #     flight_from_data$Nesting[i] <- 1
+  } else {
+    non.nesters <- c(non.nesters, i)
+  }
+  
+}
 #############################################################################################
 ############ Original script #####################
 
