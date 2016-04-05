b796734cc2561b732a70b0def5f9a477e90b34ca
Sharon Dulava <sd1249@humboldt.edu>
2016-04-05 01:00:05 -0700

    Added and created triplicate files for Bluff North

--- triplicate_nn_analysis.R
+++ triplicate_nn_analysis.R
@@ -8,9 +8,9 @@ library(PresenceAbsence)
 library(maptools)
 # Create spatial points data frames from flight 1, flight 3, and flight 4 shapefiles
 
-a <- readOGR(dsn="./TestData", layer="AWPE_F3_400_SaddleSHIFT")
-b <- readOGR(dsn="./TestData", layer="AWPE_F4_300_SaddleSHIFT")
-c <- readOGR(dsn="./TestData", layer="AWPE_F1_400_Saddle")
+a <- readOGR(dsn="./TestData", layer="AWPE_F1_400_BluffNorth")
+b <- readOGR(dsn="./TestData", layer="AWPE_F3_400_BluffNorth")
+c <- readOGR(dsn="./TestData", layer="AWPE_F4_300_BluffNorth")
 
 triplicatenn <- function(a, b, c){
   # create spatial point pattern using as.ppp function in Spatstat
@@ -113,11 +113,11 @@ a <- triplicatenn(a, b, c)
 
 sum(a$Nesting) #How many birds did it classify as nesting 
 
-png("boxplotf3f4f1saddle.png")
+png("boxplotf1f3f4bluffn.png")
 boxplot(a$dist ~ a$Nesting) ### export as a figure after some clean up? 
 dev.off()
 
-write.csv(a, "f3f4f1_nestpoints_saddle.csv")
+write.csv(a, "f1f3f4_nestpoints_bluffn.csv")
 
 
 
